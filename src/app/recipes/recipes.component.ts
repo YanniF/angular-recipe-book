6c9ae287e5f4f87b863888bb7246732be3047fb3
Yanni Fraga <Yanni.Fraga@acrontum.de>
2022-03-30 17:23:25 +0200

    Removed event emitter and added Subject

--- src/app/recipes/recipes.component.ts
+++ src/app/recipes/recipes.component.ts
@@ -1,6 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import {Recipe} from "./recipe.model";
-import {RecipesService} from "./recipes.service";
 
 @Component({
   selector: 'app-recipes',
@@ -8,13 +6,8 @@ import {RecipesService} from "./recipes.service";
   styleUrls: ['./recipes.component.css']
 })
 export class RecipesComponent implements OnInit {
-  selectedRecipe: Recipe;
-
-  constructor(private recipesService: RecipesService) { }
+  constructor() { }
 
   ngOnInit(): void {
-    this.recipesService.selectedRecipe.subscribe((recipe: Recipe) => {
-      this.selectedRecipe = recipe;
-    });
   }
 }
