ee2365a90ded26de7f3e2cd53a61f8b88fd987cc
Yanni Fraga <Yanni.Fraga@acrontum.de>
2022-04-02 16:33:55 +0200

    Shopping list edit form

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,27 +1,61 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {Ingredient} from "../../shared/ingredient.model";
 import {ShoppingListService} from "../shopping-list.service";
+import {NgForm} from "@angular/forms";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-shopping-edit',
   templateUrl: './shopping-edit.component.html',
   styleUrls: ['./shopping-edit.component.css']
 })
-export class ShoppingEditComponent implements OnInit {
-  name: string = '';
-  amount: number | undefined = undefined;
+export class ShoppingEditComponent implements OnInit, OnDestroy {
+  @ViewChild('form', { static: false }) slForm: NgForm;
+
+  subscription: Subscription;
+  editMode = false;
+  editItemIndex: number;
+  editedItem: Ingredient;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit(): void {
+    this.subscription = this.slService.startedEditing.subscribe((index: number) => {
+      this.editMode = true;
+      this.editItemIndex = index;
+      this.editedItem = this.slService.getIngredient(index);
+      this.slForm.setValue({
+        name: this.editedItem.name,
+        amount: this.editedItem.amount,
+      })
+    })
   }
 
-  onAddIngredient() {
-    this.slService.addIngredient(new Ingredient(this.name, this.amount));
+  onSubmit(form: NgForm) {
+    const value  = form.value;
+    const newIngredient = new Ingredient(value.name, value.amount)
+
+    if (this.editMode) {
+      this.slService.updateIngredient(this.editItemIndex, newIngredient);
+    }
+    else {
+      this.slService.addIngredient(newIngredient);
+    }
+
+    this.onClearForm();
+  }
+
+  onDelete() {
+    this.slService.deleteIngredient(this.editItemIndex);
+    this.onClearForm()
   }
 
   onClearForm() {
-    this.name = '';
-    this.amount = undefined;
+    this.slForm.reset();
+    this.editMode = false;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 }
